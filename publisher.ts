e86b7c54e6edd4e0bba25b951b55b43d8d57ce82
Miles Duval-Hall <miles.duval-hall@in-space.co.uk>
2024-03-08 22:40:02 +0000

    created simple pubsub server

--- publisher.ts
+++ publisher.ts
@@ -1,15 +1,27 @@
 import { io } from "socket.io-client"
+import { createInterface } from "readline"
 
 const HOST = process.env.HOST || "127.0.0.1"
 const PORT = Number(process.env.PORT) || 42069
 const URL = `http://${HOST}:${PORT}`
+const rl = createInterface({ input: process.stdin, output: process.stdout })
+
+async function getInput(): Promise<string> {
+    return new Promise((resolve) => rl.question("input: ", resolve))
+}
 
 const socket = io(URL)
 let interval: NodeJS.Timeout | undefined = undefined
 
-function handleConnect() {
+async function handleConnect() {
     console.log("connected to server on", URL)
     socket.emit("subscribe", "test-topic")
+
+    while (true) {
+        let input = await getInput()
+        console.log("publishing to test-topic ->", { message: input })
+        socket.emit("publish", "test-topic", { message: input })
+    }
 }
 
 function handleDisconnect() {
@@ -17,13 +29,8 @@ function handleDisconnect() {
     clearInterval(interval)
 }
 
-function handlePublish(topic: string, message: Object) {
-    console.log("received message from", topic, "->", message)
-}
-
 socket.on("connect", () => handleConnect())
 socket.on("disconnect", () => handleDisconnect())
-socket.on("publish", (topic: string, message: Object) => handlePublish(topic, message))
 
 process.on("SIGINT", () => {
     clearInterval(interval)
--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 import { createServer } from "http"
 import { Server, Socket } from "socket.io"
 
+const HOST = process.env.HOST || "127.0.0.1"
+const PORT = Number(process.env.PORT) || 42069
+
 const server = createServer()
 const io = new Server(server, {
     cors: {
@@ -9,12 +12,59 @@ const io = new Server(server, {
     },
 })
 
-function handleConnection(socket: Socket) {
-    console.log("connection from", socket.id)
+type Connection = {
+    id: string
+    socket: Socket
+    topics: Set<string>
 }
 
-io.on("connection", (socket) => handleConnection(socket))
+let connections: Connection[] = []
 
-server.listen(3000, () => {
-    console.log("Server listening on port 3000")
+function handleConnection(socket: Socket) {
+    console.log("connection from", socket.id)
+    connections.push({ id: socket.id, socket: socket, topics: new Set() })
+}
+
+function handleDisconnection(socket: Socket) {
+    console.log("disconnection from", socket.id)
+    connections = connections.filter((c) => c.id != socket.id)
+}
+
+function handleSubscription(socket: Socket, topic: string) {
+    console.log(socket.id, "subscribed to", topic)
+    let connection = connections.find((c) => c.id == socket.id)
+    connection?.topics.add(topic)
+}
+
+function handleUnsubscription(socket: Socket, topic: string) {
+    console.log(socket.id, "unsubscribed to", topic)
+    let connection = connections.find((c) => c.id == socket.id)
+    connection?.topics.delete(topic)
+}
+
+function handlePublish(socket: Socket, topic: string, message: Object) {
+    if (typeof message != "object") {
+        socket.emit("error", { message: "message must be an Object" })
+        console.error(socket.id, "published message must be an Object")
+        return
+    }
+
+    console.log(socket.id, "published to", topic, "->", message)
+    connections.forEach((c) => {
+        if (c.topics.has(topic)) {
+            c.socket.emit("publish", topic, message)
+        }
+    })
+}
+
+io.on("connection", (socket) => {
+    handleConnection(socket)
+    socket.on("disconnect", () => handleDisconnection(socket))
+    socket.on("subscribe", (topic: string) => handleSubscription(socket, topic))
+    socket.on("unsubscribe", (topic: string) => handleUnsubscription(socket, topic))
+    socket.on("publish", (topic: string, message: Object) => handlePublish(socket, topic, message))
+})
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server listening on ${HOST}:${PORT}`)
 })
